51dd70d45ecd2ba2635d36425ff8a3127574e83a
Saray Rivas <saraysahar.r@gmail.com>
2025-12-04 19:34:18 -0500

    .

--- inventories/serializers.py
+++ inventories/serializers.py
@@ -14,11 +14,6 @@ class WarehouseSerializer(serializers.ModelSerializer):
         fields = ('id_warehouse', 'name', 'location', 'creation_date', 'update_date',)
         model = models.Warehouse
         
-class WarehouseCreationSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        fields = ('id_warehouse_creation', 'name', 'location', 'creation_date', 'update_date', 'inventories',)
-        model = models.WarehouseCreation
 
 class ShelveSerializer(serializers.ModelSerializer):
 
@@ -32,21 +27,3 @@ class InventorySerializer(serializers.ModelSerializer):
         fields = ('id_inventory', 'id_product', 'id_warehouse', 'id_shelve', 'quantity', 'creation_date', 'update_date',)
         model = models.Inventory
 
-class InventoryMovementSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        fields = ('id_movement', 'inventory', 'movement_type', 'quantity', 'movement_date', 'notes',)
-        model = models.InventoryMovement
-
-class OrderCreationSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        fields = ('id_order_creation', 'order_number', 'status','quantity', 'creation_date', 'update_date', 'product_name', 'inventories',)
-        model = models.OrderCreation
-
-class AuditLogSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        fields = ('order', 'user', 'action_type', 'detail', 'created_at',)
-        model = models.AuditLog
-    
